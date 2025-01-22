559a179c36b7e95e001de683a179c72a42aef5da
gwin <gwin@kali>
2025-01-22 22:20:37 +0100

    served a file using the filePath

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 let express = require('express');
+let path = require("path")
 let app = express();
 
 console.log("Hello Express")
 
 app.get("/", function (req, res) {
-    res.send("Hello Express");
+    const filePath = path.join(__dirname, "/views/index.html")
+    res.sendFile(filePath);
 })
 
 
