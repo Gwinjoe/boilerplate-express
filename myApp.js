6e6251119f6dd4077d03447e741e1999ef85130c
gwin <gwin@kali>
2025-01-23 07:43:05 +0100

    served first json

--- myApp.js
+++ myApp.js
@@ -13,7 +13,11 @@ app.get("/", function (req, res) {
 app.use("/public", express.static(path.join(__dirname, "/public")))
 
 
-
+app.get("/json", function (req, res) {
+    res.json({
+        message: "Hello json"
+    })
+})
 
 
 
